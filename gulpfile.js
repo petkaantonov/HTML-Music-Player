c2dec106d3bca454074d8c4da812d06bd39f1fd6
Petka Antonov <petka.antonov@reaktor.fi>
2016-03-14 14:07:31 +0200

    worker bundles

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var del = require('del');
+var mkdirp = require("mkdirp").sync;
+var glob = require("glob").sync;
 var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 var rollup = require('rollup-stream');
 var uglify = require('rollup-plugin-uglify');
 var gjslint = require('gulp-gjslint');
@@ -14,6 +15,10 @@ var Promise = require("bluebird");
 var includePaths = require("rollup-plugin-includepaths");
 var commonjs = require("rollup-plugin-commonjs");
 var nodeResolve = require("rollup-plugin-node-resolve");
+var gulpUglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+
+var licenseHeader = fs.readFileSync("./LICENSE_header", "utf8");
 
 function bundleJs(opts) {
     opts = Object(opts);
@@ -63,7 +68,7 @@ function bundleGui() {
         entry: "src/bootstrap.js",
         moduleName: "bootstrap",
         format: "iife",
-        banner: "/* license */",
+        banner: licenseHeader,
         min: true
     });
 
@@ -81,4 +86,95 @@ function bundleGui() {
     return Promise.all([full, minified]);
 }
 
+function bundleCodecs() {
+    mkdirp("dist/worker/codecs");
+    var codecs = glob("src/codecs/**/*.js");
+
+    return Promise.map(codecs, function(codecPath) {
+        var name = path.basename(codecPath, ".js");
+        var bundles = bundleJs({
+            entry: codecPath,
+            moduleName: name,
+            format: "iife",
+            /*banner: todo*/
+            min: true
+        });
+
+        var full = bundles.full.pipe(source(name + ".js"))
+                            .pipe(buffer())
+                            .pipe(sourcemaps.init({loadMaps: true}))
+                            .pipe(sourcemaps.write("."))
+                            .pipe(gulp.dest("dist/worker/codecs"));
+
+        var min = bundles.minified.pipe(source(name + ".min.js"))
+                            .pipe(buffer())
+                            .pipe(sourcemaps.init({loadMaps: true}))
+                            .pipe(sourcemaps.write("."))
+                            .pipe(gulp.dest("dist/worker/codecs"));
+
+        return Promise.all([min, full]);
+    }, {concurrency: 4});
+}
+
+function bundleWorker(entry, name) {
+    mkdirp("dist/worker");
+
+    var bundles = bundleJs({
+        entry: entry,
+        moduleName: name,
+        format: "iife",
+        banner: licenseHeader
+    });
+
+    var full = bundles.full.pipe(source(name + ".js"))
+                .pipe(buffer())
+                .pipe(sourcemaps.init({loadMaps: true}))
+                .pipe(replace(/^/, "self.DEBUGGING = true;\n"))
+                .pipe(sourcemaps.write("."))
+                .pipe(gulp.dest("dist/worker"));
+
+    var minified = bundles.minified.pipe(source(name + ".min.js"))
+                    .pipe(buffer())
+                    .pipe(sourcemaps.init({loadMaps: true}))
+                    .pipe(replace(/^/, "self.DEBUGGING = false;\n"))
+                    .pipe(sourcemaps.write("."))
+                    .pipe(gulp.dest("dist/worker"));
+
+    return Promise.all([full, minified]);
+}
+
+function bundleTrackAnalyzerBackend() {
+    return bundleWorker("src/audio/TrackAnalyzerBackend.js", "TrackAnalyzerBackend");
+}
+
+function bundleAudioPlayerBackend() {
+    return bundleWorker("src/audio/AudioPlayerBackend.js", "AudioPlayerBackend");
+}
+
+function bundleServiceWorker() {
+    var assets = ["dist/css/app-css-public.min.css"]
+                    .concat(glob("dist/images/**/*.*"))
+                    .concat(glob("dist/fonts/**/*.woff*"))
+                    .concat(glob("dist/worker/codecs/**/*.min.js"))
+                    .concat("dist/worker/AudioPlayerWorker.min.js", "dist/worker/TrackAnalyzerWorker.min.js");
+
+    var serviceWorkerAssetsList = assets.concat("dist/main.min.js", "index.html", "/").sort();
+    var assetsCode = "const assets = " + JSON.stringify(serviceWorkerAssetsList, null, 4) + ";\n";
+    var buildDate = "const buildDate = '" + new Date().toUTCString()+ "';\n";
+    var code = "// AUTOMATICALLY GENERATED FILE DO NOT EDIT\n" + licenseHeader + assetsCode +  buildDate;
+
+    return gulp.src("sw_base.js")
+                .pipe(rename("sw.js"))
+                .pipe(buffer())
+                .pipe(replace(/^/, code))
+                .pipe(sourcemaps.init({loadMaps: true}))
+                .pipe(gulpUglify({preserveComments: "license"}))
+                .pipe(sourcemaps.write("."))
+                .pipe(gulp.dest("."));
+}
+
+gulp.task("audio-player-worker", bundleAudioPlayerBackend);
+gulp.task("track-analyzer-worker", bundleTrackAnalyzerBackend);
+gulp.task("service-worker", bundleServiceWorker);
+gulp.task("codecs", bundleCodecs);
 gulp.task("gui", bundleGui);
