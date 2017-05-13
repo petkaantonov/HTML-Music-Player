96e3973f2777640d28481f150c98a4e024607367
Petka Antonov <petka_antonov@hotmail.com>
2017-05-13 23:37:07 +0300

    Use es6 class syntax

--- server/ytdl.js
+++ server/ytdl.js
@@ -142,277 +142,278 @@ class GrowingAudioFileReadable extends Readable {
     }
 }
 
-function YoutubeDl(ytid) {
-    EventEmitter.call(this);
-    this.ytid = ytid;
-    this.handle = null;
-    this.aborted = false;
-    this.uid = (Math.random() + "").replace(/[^0-9]+/g, "");
-    this.started = -1;
-    this.audioFileData = null;
-    this.logHandle = Promise.resolve();
-    this.readStreams = [];
-}
-util.inherits(YoutubeDl, EventEmitter);
-
-YoutubeDl.prototype.getDummyData = function() {
-    return {
-        duration: 0,
-        title: "",
-        realSize: -1,
-        predictedSize: 0,
-        bitRate: 0,
-        handle: this.getHandle()
-    };
-};
-
-YoutubeDl.prototype.getHandle = function() {
-    return this.uid + "-" + this.ytid;
-};
-
-YoutubeDl.prototype.getDataDir = function() {
-    return path.join(DATA_ROOT, "data-" + this.getHandle());
-};
-
-YoutubeDl.prototype.getAudioFileName = function() {
-    return this.ytid + ".mp3";
-};
-
-YoutubeDl.prototype.getAudioFilePath = function() {
-    return path.join(this.getDataDir(), this.getAudioFileName());
-};
-
-YoutubeDl.prototype.getInfoFilePath = function() {
-    return path.join(this.getDataDir(), "-.info.json");
-};
-
-YoutubeDl.prototype.getCookieFilePath = function() {
-    return path.join(this.getDataDir(), "cookie.txt");
-};
-
-YoutubeDl.prototype.getLogFilePath = function() {
-    return path.join(this.getDataDir(), "output.log");
-};
-
-YoutubeDl.prototype.log = function(message) {
-    this.logHandle = this.logHandle.then(handle => {
-        if (handle) {
-            message = message instanceof Buffer ? message : Buffer.from(message + "", "utf8");
-            var stream = fs.createWriteStream(null, {
-                flags: "a",
-                fd: handle,
-                autoClose: false
-            });
-            return stream.endAsync(message).thenReturn(handle);
-        }
-    });
-};
-
-YoutubeDl.prototype.closeLogFile = function() {
-    return this.logHandle.then(handle => {
+class YoutubeDl extends EventEmitter {
+    constructor(ytid) {
+        super();
+        this.ytid = ytid;
+        this.handle = null;
+        this.aborted = false;
+        this.uid = (Math.random() + "").replace(/[^0-9]+/g, "");
+        this.started = -1;
+        this.audioFileData = null;
         this.logHandle = Promise.resolve();
-        if (handle) {
-            return fs.closeAsync(handle);
-        }
-    });
-};
-
-YoutubeDl.prototype.closeStreams = function() {
-    this.readStreams.forEach(readStream => readStream.abort());
-    this.readStreams = [];
-};
-
-YoutubeDl.prototype.cleanup = function() {
-    if (this.aborted) return;
-    this.aborted = true;
-    this.closeLogFile();
-    this.closeStreams();
-    if (this.handle) {
-        return new Promise(resolve => {
-            this.handle.on("exit", resolve);
-        }).then(() => rimrafAsync(this.getDataDir()));
-    } else {
-        return rimrafAsync(this.getDataDir());
+        this.readStreams = [];
     }
-};
 
-// Client aborting the download process
-YoutubeDl.prototype.abort = function() {
-    if (this.aborted) return;
-    if (this.handle) this.handle.kill();
-    return this.cleanup();
-};
+    getDummyData() {
+        return {
+            duration: 0,
+            title: "",
+            realSize: -1,
+            predictedSize: 0,
+            bitRate: 0,
+            handle: this.getHandle()
+        };
+    }
 
-// Client starting the download process, returns a handle for the client from which .streamAudio() can immediately
-// be used to start streaming the audio.
-YoutubeDl.prototype.start = function() {
-    if (this.handle || this.aborted) return;
-    this.started = Date.now();
-    return mkdirpAsync(this.getDataDir()).then(() => {
-        if (this.aborted) {
+    getHandle() {
+        return this.uid + "-" + this.ytid;
+    }
+
+    getDataDir() {
+        return path.join(DATA_ROOT, "data-" + this.getHandle());
+    }
+
+    getAudioFileName() {
+        return this.ytid + ".mp3";
+    }
+
+    getAudioFilePath() {
+        return path.join(this.getDataDir(), this.getAudioFileName());
+    }
+
+    getInfoFilePath() {
+        return path.join(this.getDataDir(), "-.info.json");
+    }
+
+    getCookieFilePath() {
+        return path.join(this.getDataDir(), "cookie.txt");
+    }
+
+    getLogFilePath() {
+        return path.join(this.getDataDir(), "output.log");
+    }
+
+    log(message) {
+        this.logHandle = this.logHandle.then(handle => {
+            if (handle) {
+                message = message instanceof Buffer ? message : Buffer.from(message + "", "utf8");
+                var stream = fs.createWriteStream(null, {
+                    flags: "a",
+                    fd: handle,
+                    autoClose: false
+                });
+                return stream.endAsync(message).thenReturn(handle);
+            }
+        });
+    }
+
+    closeLogFile() {
+        return this.logHandle.then(handle => {
+            this.logHandle = Promise.resolve();
+            if (handle) {
+                return fs.closeAsync(handle);
+            }
+        });
+    }
+
+    closeStreams() {
+        this.readStreams.forEach(readStream => readStream.abort());
+        this.readStreams = [];
+    }
+
+    cleanup() {
+        if (this.aborted) return;
+        this.aborted = true;
+        this.closeLogFile();
+        this.closeStreams();
+        if (this.handle) {
+            return new Promise(resolve => {
+                this.handle.on("exit", resolve);
+            }).then(() => rimrafAsync(this.getDataDir()));
+        } else {
             return rimrafAsync(this.getDataDir());
         }
-        this.logHandle = fs.openAsync(this.getLogFilePath(), "a");
-        return new Promise((resolve, reject) => {
-            var stderr = "";
-            var cwd = this.getDataDir();
-            var shellCommand = [
-                "youtube-dl",
-                "--ignore-config",
-                "--no-color",
-                "--fixup",
-                "never",
-                "--no-mark-watched",
-                "--no-playlist",
-                "--no-mtime",
-                "-o",
-                "-",
-                "--restrict-filenames",
-                "-w",
-                "--write-info-json",
-                "--cache-dir",
-                CACHE_ROOT,
-                "--cookies",
-                this.getCookieFilePath(),
-                "--no-progress",
-                "--no-call-home",
-                "--no-check-certificate",
-                "--format",
-                "258/256/141/140/139/172/171/251/250/249/22/34/35/43/44/59/78/best",
-                "--no-post-overwrites",
-                "https://www.youtube.com/watch?v=" + this.ytid,
-                "|",
-                "/usr/bin/ffmpeg",
-                "-y",
-                "-i",
-                "pipe:0",
-                "-vn",
-                "-acodec",
-                "libmp3lame",
-                "-b:a",
-                (OUTPUT_BITRATE / 1000) + "k",
-                "file:" + this.getAudioFileName()].join(" ");
+    }
 
-            this.emit("downloadStart");
-            this.handle = spawn("sh", ["-c", shellCommand], {
-                cwd: cwd
-            });
+    // Client aborting the download process
+    abort() {
+        if (this.aborted) return;
+        if (this.handle) this.handle.kill();
+        return this.cleanup();
+    }
 
-            this.handle.stderr.on("data", (data) => {
-                stderr += data;
-                this.log(data);
-                // TODO: Use streaming parser to detect when info is ready.
-                if (!this.infoRetrieved && stderr.indexOf("[download] Destination:") >= 0) {
-                    this.infoRetrieved = true;
-                    resolve(fs.readFileAsync(this.getInfoFilePath(), "utf8").then(result => {
-                        if (this.aborted) {
-                            return this.getDummyData();
-                        }
-                        result = JSON.parse(result);
-                        var self = this;
-                        function statLoop() {
-                            return fs.statAsync(self.getAudioFilePath()).catch(e => {
-                                if (self.aborted) {
-                                    return {size: 0};
-                                }
-                                if (e && e.code === "ENOENT") {
-                                    return Promise.delay(10).then(statLoop);
-                                }
-                                throw e;
-                            });
-                        }
+    // Client starting the download process, returns a handle for the client from which .streamAudio() can immediately
+    // be used to start streaming the audio.
+    start() {
+        if (this.handle || this.aborted) return;
+        this.started = Date.now();
+        return mkdirpAsync(this.getDataDir()).then(() => {
+            if (this.aborted) {
+                return rimrafAsync(this.getDataDir());
+            }
+            this.logHandle = fs.openAsync(this.getLogFilePath(), "a");
+            return new Promise((resolve, reject) => {
+                var stderr = "";
+                var cwd = this.getDataDir();
+                var shellCommand = [
+                    "youtube-dl",
+                    "--ignore-config",
+                    "--no-color",
+                    "--fixup",
+                    "never",
+                    "--no-mark-watched",
+                    "--no-playlist",
+                    "--no-mtime",
+                    "-o",
+                    "-",
+                    "--restrict-filenames",
+                    "-w",
+                    "--write-info-json",
+                    "--cache-dir",
+                    CACHE_ROOT,
+                    "--cookies",
+                    this.getCookieFilePath(),
+                    "--no-progress",
+                    "--no-call-home",
+                    "--no-check-certificate",
+                    "--format",
+                    "258/256/141/140/139/172/171/251/250/249/22/34/35/43/44/59/78/best",
+                    "--no-post-overwrites",
+                    "https://www.youtube.com/watch?v=" + this.ytid,
+                    "|",
+                    "/usr/bin/ffmpeg",
+                    "-y",
+                    "-i",
+                    "pipe:0",
+                    "-vn",
+                    "-acodec",
+                    "libmp3lame",
+                    "-b:a",
+                    (OUTPUT_BITRATE / 1000) + "k",
+                    "file:" + this.getAudioFileName()].join(" ");
 
-                        return Promise.delay(10).then(statLoop).then(() => {
+                this.emit("downloadStart");
+                this.handle = spawn("sh", ["-c", shellCommand], {
+                    cwd: cwd
+                });
+
+                this.handle.stderr.on("data", (data) => {
+                    stderr += data;
+                    this.log(data);
+                    // TODO: Use streaming parser to detect when info is ready.
+                    if (!this.infoRetrieved && stderr.indexOf("[download] Destination:") >= 0) {
+                        this.infoRetrieved = true;
+                        resolve(fs.readFileAsync(this.getInfoFilePath(), "utf8").then(result => {
                             if (this.aborted) {
                                 return this.getDummyData();
                             }
+                            result = JSON.parse(result);
+                            var self = this;
+                            function statLoop() {
+                                return fs.statAsync(self.getAudioFilePath()).catch(e => {
+                                    if (self.aborted) {
+                                        return {size: 0};
+                                    }
+                                    if (e && e.code === "ENOENT") {
+                                        return Promise.delay(10).then(statLoop);
+                                    }
+                                    throw e;
+                                });
+                            }
 
-                            var bitRate = OUTPUT_BITRATE;
-                            var predictedSize = (bitRate * result.duration) / 8;
+                            return Promise.delay(10).then(statLoop).then(() => {
+                                if (this.aborted) {
+                                    return this.getDummyData();
+                                }
 
-                            this.audioFileData = {
-                                duration: result.duration,
-                                title: result.title,
-                                predictedSize: predictedSize,
-                                realSize: -1,
-                                bitRate: bitRate,
-                                handle: this.getHandle()
-                            };
+                                var bitRate = OUTPUT_BITRATE;
+                                var predictedSize = (bitRate * result.duration) / 8;
 
-                            return this.audioFileData;
-                        });
-                    }));
-                }
-            });
+                                this.audioFileData = {
+                                    duration: result.duration,
+                                    title: result.title,
+                                    predictedSize: predictedSize,
+                                    realSize: -1,
+                                    bitRate: bitRate,
+                                    handle: this.getHandle()
+                                };
 
-            this.handle.on("exit", (code) => {
-                this.emit("downloadEnd");
-                var success = (+code) === 0;
-                this.handle = null;
-                this.log("exited with code " + code);
-                if (!success) {
-                    reject(new Error(stderr));
-                } else if (!this.aborted) {
-                    fs.statAsync(this.getAudioFilePath()).then(stat => {
-                        if (this.audioFileData) {
-                            this.audioFileData.realSize = stat.size;
-                        }
-                    }).catch(() => {});
-                }
-                this.closeLogFile();
-            });
+                                return this.audioFileData;
+                            });
+                        }));
+                    }
+                });
 
-            this.handle.on("error", (error) => {
-                this.log(error && error.stack || error + "");
+                this.handle.on("exit", (code) => {
+                    this.emit("downloadEnd");
+                    var success = (+code) === 0;
+                    this.handle = null;
+                    this.log("exited with code " + code);
+                    if (!success) {
+                        reject(new Error(stderr));
+                    } else if (!this.aborted) {
+                        fs.statAsync(this.getAudioFilePath()).then(stat => {
+                            if (this.audioFileData) {
+                                this.audioFileData.realSize = stat.size;
+                            }
+                        }).catch(() => {});
+                    }
+                    this.closeLogFile();
+                });
+
+                this.handle.on("error", (error) => {
+                    this.log(error && error.stack || error + "");
+                });
             });
         });
-    });
-};
-
-// Client has fully downloaded the file to IndexedDB
-YoutubeDl.prototype.confirmDownload = function() {
-    return this.cleanup();
-};
-
-YoutubeDl.prototype.getAudioFileSize = function(overEstimate) {
-    if (!this.audioFileData) {
-        throw new Error("invalid call");
     }
-    if (this.audioFileData.realSize >= 0) {
-        return this.audioFileData.realSize;
-    } else {
-        return this.audioFileData.predictedSize + (overEstimate ? OUTPUT_BITRATE / 8 * 5 : 0);
-    }
-};
 
-YoutubeDl.prototype.isAudioFileFinished = function() {
-    return this.audioFileData && this.audioFileData.realSize >= 0;
-};
-
-YoutubeDl.prototype.isDownloading = function() {
-    if (this.handle || !this.isAudioFileFinished()) {
-        return !this.aborted;
+    // Client has fully downloaded the file to IndexedDB
+    confirmDownload() {
+        return this.cleanup();
     }
-    return false;
-};
 
-// Client requesting a chunk of the file
-YoutubeDl.prototype.streamAudio = function(offset, length) {
-    if (this.aborted || !this.audioFileData) {
-        throw new Error("invalid call");
-    }
-    var ret = new GrowingAudioFileReadable(this, offset, length);
-    ret.on("end", () => {
-        var index = this.readStreams.indexOf(ret);
-        if (index >= 0) {
-            this.readStreams.splice(index, 1);
+    getAudioFileSize(overEstimate) {
+        if (!this.audioFileData) {
+            throw new Error("invalid call");
         }
-    });
-    this.readStreams.push(ret);
-    while (this.readStreams.length > MAX_CONCURRENT_READ_STREAMS) {
-        this.readStreams.shift().abort();
+        if (this.audioFileData.realSize >= 0) {
+            return this.audioFileData.realSize;
+        } else {
+            return this.audioFileData.predictedSize + (overEstimate ? OUTPUT_BITRATE / 8 * 5 : 0);
+        }
     }
-    return ret;
-};
+
+    isAudioFileFinished() {
+        return this.audioFileData && this.audioFileData.realSize >= 0;
+    }
+
+    isDownloading() {
+        if (this.handle || !this.isAudioFileFinished()) {
+            return !this.aborted;
+        }
+        return false;
+    }
+
+    // Client requesting a chunk of the file
+    streamAudio(offset, length) {
+        if (this.aborted || !this.audioFileData) {
+            throw new Error("invalid call");
+        }
+        var ret = new GrowingAudioFileReadable(this, offset, length);
+        ret.on("end", () => {
+            var index = this.readStreams.indexOf(ret);
+            if (index >= 0) {
+                this.readStreams.splice(index, 1);
+            }
+        });
+        this.readStreams.push(ret);
+        while (this.readStreams.length > MAX_CONCURRENT_READ_STREAMS) {
+            this.readStreams.shift().abort();
+        }
+        return ret;
+    }
+}
 
 module.exports = YoutubeDl;
