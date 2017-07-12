ed7ed5038cd265552c34df1cb2c3c7717689b27f
Petka Antonov <petka_antonov@hotmail.com>
2017-07-12 23:15:22 +0300

    fix inherit bug

--- src/search/SearchResultTrackView.js
+++ src/search/SearchResultTrackView.js
@@ -1,32 +1,30 @@
-
-
 import TrackView from "tracks/TrackView";
-import {inherits} from "util";
 
-export default function SearchResultTrackView(track, result, opts) {
-    TrackView.call(this, track, opts);
-    this._result = result;
-    if (track._isDisplayedAsSearchResult) {
-        throw new Error(`duplicate search result view for this track`);
+export default class SearchResultTrackView extends TrackView {
+    constructor (track, result, opts) {
+        super(track, opts);
+        this._result = result;
+        if (track._isDisplayedAsSearchResult) {
+            throw new Error(`duplicate search result view for this track`);
+        }
+        track._isDisplayedAsSearchResult = true;
     }
-    track._isDisplayedAsSearchResult = true;
+
+    search() {
+        return this._opts.search;
+    }
+
+    destroy() {
+        if (!this.destroy$()) return false;
+        if (!this._track._isDisplayedAsSearchResult) {
+            throw new Error(`track is not displayed as search result`);
+        }
+        this._track._isDisplayedAsSearchResult = false;
+        this._result = null;
+        return true;
+    }
+
+    viewUpdateDestroyed() {
+        // NOOP
+    }
 }
-inherits(SearchResultTrackView, TrackView);
-
-SearchResultTrackView.prototype.search = function() {
-    return this._opts.search;
-};
-
-SearchResultTrackView.prototype.destroy = function() {
-    if (!this.destroy$()) return false;
-    if (!this._track._isDisplayedAsSearchResult) {
-        throw new Error(`track is not displayed as search result`);
-    }
-    this._track._isDisplayedAsSearchResult = false;
-    this._result = null;
-    return true;
-};
-
-SearchResultTrackView.prototype.viewUpdateDestroyed = function() {
-    // NOOP
-};
