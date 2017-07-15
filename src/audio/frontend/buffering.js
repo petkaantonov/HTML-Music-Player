79d199bb763d536ae75de982f600abd11e0cdb50
Petka Antonov <petka_antonov@hotmail.com>
2017-07-15 19:20:44 +0300

    default buffer 300ms

--- src/audio/frontend/buffering.js
+++ src/audio/frontend/buffering.js
@@ -1,7 +1,7 @@
 export const FLOAT32_BYTES = 4;
 export const WEB_AUDIO_BLOCK_SIZE = 128;
 export const SCHEDULE_AHEAD_RATIO = 0.75;
-export const DEFAULT_BUFFER_LENGTH_SECONDS = 0.5;
+export const DEFAULT_BUFFER_LENGTH_SECONDS = 0.3;
 export const MIN_BUFFER_LENGTH_SECONDS = 0.1;
 export const MAX_BUFFER_LENGTH_SECONDS = 2;
 export const SUSTAINED_BUFFERED_AUDIO_RATIO = 5;
