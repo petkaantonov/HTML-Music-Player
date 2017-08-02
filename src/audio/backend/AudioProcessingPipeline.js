66924fa63b4a3fe01e87ed66759d6765ec252499
Petka Antonov <petka_antonov@hotmail.com>
2017-08-03 01:07:45 +0300

    do loudness analysis after effects

--- src/audio/backend/AudioProcessingPipeline.js
+++ src/audio/backend/AudioProcessingPipeline.js
@@ -185,15 +185,15 @@ export default class AudioProcessingPipeline {
         };
         let loudnessInfo = defaultLoudnessInfo;
 
-        if (loudnessAnalyzer) {
-            const audioFrameLength = byteLength / sourceChannelCount / I16_BYTE_LENGTH;
-            loudnessInfo = loudnessAnalyzer.applyLoudnessNormalization(samplePtr, audioFrameLength);
-        }
-
         if (effects) {
             for (const effect of effects) {
                 ({samplePtr, byteLength} = effect.apply(effects, samplePtr, byteLength, metadata));
             }
+        }
+
+        if (loudnessAnalyzer) {
+            const audioFrameLength = byteLength / sourceChannelCount / I16_BYTE_LENGTH;
+            loudnessInfo = loudnessAnalyzer.applyLoudnessNormalization(samplePtr, audioFrameLength);
         }
 
         if (crossfader) {
